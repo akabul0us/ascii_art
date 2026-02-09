5fcad2ca63bda7bb030e4522cda74b31ea4b83e4
akabul0us <jeremywokka@proton.me>
2026-02-09 13:19:10 -0600

    added gorgotron, renamed files

---
+++ fastfetch_commands.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+fastfetch --file gorgotron_ff.ascii --logo-color-1 magenta --logo-color-2 cyan --logo-color-3 white --logo-color-4 blue --logo-padding-left 0 --logo-padding-right 1
+fastfetch --file ignignokt_ff.ascii --logo-color-1 green --logo-color-2 cyan --logo-padding-left 0 --logo-padding-right 1
+fastfetch --file er_ff.ascii --logo-color-1 magenta --logo-color-2 cyan
+fastfetch --file mooninites_ff.ascii --logo-color-1 green --logo-color-2 blue --logo-color-3 magenta --logo-color-4 cyan
+
